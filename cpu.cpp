1e19da3685e0a2ac70ed31596558acb9b29758c5
CloudingYu <24300240045@m.fudan.edu.cn>
2025-12-03 16:04:44 +0800

    update

--- cpu.cpp
+++ cpu.cpp
@@ -1,5 +1,5 @@
 #include "cpu.h"
-#include "json.hpp" 
+#include "json.hpp"
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -7,16 +7,16 @@
 
 using json = nlohmann::json;
 
-// --- 初始化 ---
-Simulator::Simulator() {
+Simulator::Simulator()
+{
     PC = 0;
     memory.resize(MEM_SIZE, 0);
     reg.fill(0);
     stat = Stat::AOK;
 }
 
-// 辅助：16进制转字节
-unsigned char hexToByte(const std::string& hex) {
+unsigned char hexToByte(const std::string &hex)
+{
     unsigned int val;
     std::stringstream ss;
     ss << std::hex << hex;
@@ -24,15 +24,17 @@ unsigned char hexToByte(const std::string& hex) {
     return static_cast<unsigned char>(val);
 }
 
-// --- 读取 .yo 文件 ---
-void Simulator::loadProgram() {
+void Simulator::loadProgram()
+{
     std::string line;
-    while (std::getline(std::cin, line)) {
+    while (std::getline(std::cin, line))
+    {
         size_t addrPos = line.find("0x");
         size_t colonPos = line.find(":");
         size_t pipePos = line.find("|");
 
-        if (addrPos != std::string::npos && colonPos != std::string::npos) {
+        if (addrPos != std::string::npos && colonPos != std::string::npos)
+        {
             std::string addrStr = line.substr(addrPos, colonPos - addrPos);
             long long addr = 0;
             std::stringstream ss;
@@ -41,16 +43,21 @@ void Simulator::loadProgram() {
 
             size_t dataEnd = (pipePos == std::string::npos) ? line.length() : pipePos;
             std::string dataStr = line.substr(colonPos + 1, dataEnd - (colonPos + 1));
-            
+
             std::string cleanData;
-            for (char c : dataStr) {
-                if (!isspace(c)) cleanData += c;
+            for (char c : dataStr)
+            {
+                if (!isspace(c))
+                    cleanData += c;
             }
 
-            for (size_t i = 0; i < cleanData.length(); i += 2) {
-                if (i + 1 < cleanData.length()) {
+            for (size_t i = 0; i < cleanData.length(); i += 2)
+            {
+                if (i + 1 < cleanData.length())
+                {
                     std::string byteStr = cleanData.substr(i, 2);
-                    if (addr < MEM_SIZE) {
+                    if (addr < MEM_SIZE)
+                    {
                         memory[addr] = hexToByte(byteStr);
                         addr++;
                     }
@@ -60,87 +67,91 @@ void Simulator::loadProgram() {
     }
 }
 
-// --- 内存辅助 ---
-long long Simulator::readLong(long long addr) {
-    if (addr < 0 || addr + 8 > MEM_SIZE) {
+long long Simulator::readLong(long long addr)
+{
+    if (addr < 0 || addr + 8 > MEM_SIZE)
+    {
         stat = Stat::ADR;
         return 0;
     }
     long long val = 0;
-    for (int i = 0; i < 8; ++i) {
+    for (int i = 0; i < 8; ++i)
+    {
         val |= (long long)memory[addr + i] << (i * 8);
     }
-    return val; 
+    return val;
 }
 
-void Simulator::writeLong(long long addr, long long val) {
-    if (addr < 0 || addr + 8 > MEM_SIZE) {
+void Simulator::writeLong(long long addr, long long val)
+{
+    if (addr < 0 || addr + 8 > MEM_SIZE)
+    {
         stat = Stat::ADR;
         return;
     }
-    for (int i = 0; i < 8; ++i) {
+    for (int i = 0; i < 8; ++i)
+    {
         memory[addr + i] = (val >> (i * 8)) & 0xFF;
     }
 }
 
-// ==========================================
-//               核心流水线阶段
-// ==========================================
-
-// 1. 取指
-void Simulator::fetch() {
-    if (PC < 0 || PC >= MEM_SIZE) {
+void Simulator::fetch()
+{
+    if (PC < 0 || PC >= MEM_SIZE)
+    {
         stat = Stat::ADR;
         return;
     }
-    
+
     unsigned char byte0 = memory[PC];
     icode = (byte0 >> 4) & 0xF;
     ifun = byte0 & 0xF;
-    
-    // 检查非法指令
-    if (icode > 0xB) {
+
+    if (icode > 0xB)
+    {
         stat = Stat::INS;
         return;
     }
 
     valP = PC + 1;
 
-    // 需要寄存器指示符的指令
-    bool need_regs = (icode == ICode::RRMOVQ || icode == ICode::OPQ || 
-                      icode == ICode::PUSHQ  || icode == ICode::POPQ || 
-                      icode == ICode::IRMOVQ || icode == ICode::RMMOVQ || 
+    bool need_regs = (icode == ICode::RRMOVQ || icode == ICode::OPQ ||
+                      icode == ICode::PUSHQ || icode == ICode::POPQ ||
+                      icode == ICode::IRMOVQ || icode == ICode::RMMOVQ ||
                       icode == ICode::MRMOVQ);
 
-    if (need_regs) {
+    if (need_regs)
+    {
         unsigned char byte1 = memory[valP];
         rA = (byte1 >> 4) & 0xF;
         rB = byte1 & 0xF;
         valP += 1;
-    } else {
+    }
+    else
+    {
         rA = Reg::NONE;
         rB = Reg::NONE;
     }
 
-    // 需要常数的指令
-    bool need_valC = (icode == ICode::IRMOVQ || icode == ICode::RMMOVQ || 
-                      icode == ICode::MRMOVQ || icode == ICode::JXX || 
+    bool need_valC = (icode == ICode::IRMOVQ || icode == ICode::RMMOVQ ||
+                      icode == ICode::MRMOVQ || icode == ICode::JXX ||
                       icode == ICode::CALL);
 
-    if (need_valC) {
+    if (need_valC)
+    {
         valC = readLong(valP);
         valP += 8;
     }
 }
 
-// 2. 译码
-void Simulator::decode() {
+void Simulator::decode()
+{
     int srcA = Reg::NONE;
     if (icode == ICode::RRMOVQ || icode == ICode::RMMOVQ || icode == ICode::OPQ || icode == ICode::PUSHQ)
         srcA = rA;
     else if (icode == ICode::POPQ || icode == ICode::RET)
         srcA = Reg::RSP;
-    
+
     valA = (srcA == Reg::NONE) ? 0 : reg[srcA];
 
     int srcB = Reg::NONE;
@@ -152,117 +163,165 @@ void Simulator::decode() {
     valB = (srcB == Reg::NONE) ? 0 : reg[srcB];
 }
 
-// 3. 执行
-void Simulator::execute() {
-    if (icode == ICode::OPQ) {
+void Simulator::execute()
+{
+    if (icode == ICode::OPQ)
+    {
         long long a = valA;
         long long b = valB;
-        if (ifun == 0) valE = b + a;      // addq
-        else if (ifun == 1) valE = b - a; // subq
-        else if (ifun == 2) valE = b & a; // andq
-        else if (ifun == 3) valE = b ^ a; // xorq
-        
+        if (ifun == 0)
+            valE = b + a;
+        else if (ifun == 1)
+            valE = b - a;
+        else if (ifun == 2)
+            valE = b & a;
+        else if (ifun == 3)
+            valE = b ^ a; // xorq
+
         zf = (valE == 0);
         sf = (valE < 0);
-        if (ifun == 0)      of = ((a > 0 && b > 0 && valE < 0) || (a < 0 && b < 0 && valE >= 0));
-        else if (ifun == 1) of = ((b > 0 && a < 0 && valE < 0) || (b < 0 && a > 0 && valE >= 0));
-        else                of = false;
+        if (ifun == 0)
+            of = ((a > 0 && b > 0 && valE < 0) || (a < 0 && b < 0 && valE >= 0));
+        else if (ifun == 1)
+            of = ((b > 0 && a < 0 && valE < 0) || (b < 0 && a > 0 && valE >= 0));
+        else
+            of = false;
     }
-    else if (icode == ICode::IRMOVQ) {
+    else if (icode == ICode::IRMOVQ)
+    {
         valE = valC;
     }
-    else if (icode == ICode::RRMOVQ) { 
+    else if (icode == ICode::RRMOVQ)
+    {
         valE = valA;
     }
-    else if (icode == ICode::RMMOVQ || icode == ICode::MRMOVQ) {
+    else if (icode == ICode::RMMOVQ || icode == ICode::MRMOVQ)
+    {
         valE = valB + valC;
     }
-    else if (icode == ICode::PUSHQ || icode == ICode::CALL) {
+    else if (icode == ICode::PUSHQ || icode == ICode::CALL)
+    {
         valE = valB - 8;
     }
-    else if (icode == ICode::POPQ || icode == ICode::RET) {
+    else if (icode == ICode::POPQ || icode == ICode::RET)
+    {
         valE = valB + 8;
     }
 
-    if (icode == ICode::JXX || icode == ICode::RRMOVQ) {
-        switch (ifun) {
-            case 0: cnd = true; break; 
-            case 1: cnd = (sf ^ of) || zf; break; 
-            case 2: cnd = (sf ^ of); break; 
-            case 3: cnd = zf; break; 
-            case 4: cnd = !zf; break; 
-            case 5: cnd = !(sf ^ of); break; 
-            case 6: cnd = !(sf ^ of) && !zf; break; 
-            default: cnd = false;
+    if (icode == ICode::JXX || icode == ICode::RRMOVQ)
+    {
+        switch (ifun)
+        {
+        case 0:
+            cnd = true;
+            break;
+        case 1:
+            cnd = (sf ^ of) || zf;
+            break;
+        case 2:
+            cnd = (sf ^ of);
+            break;
+        case 3:
+            cnd = zf;
+            break;
+        case 4:
+            cnd = !zf;
+            break;
+        case 5:
+            cnd = !(sf ^ of);
+            break;
+        case 6:
+            cnd = !(sf ^ of) && !zf;
+            break;
+        default:
+            cnd = false;
         }
     }
 
-    if (icode == ICode::HALT) stat = Stat::HLT;
+    if (icode == ICode::HALT)
+        stat = Stat::HLT;
 }
 
-// 4. 访存
-void Simulator::memory_access() {
+void Simulator::memory_access()
+{
     long long addr = 0;
     bool read = false, write = false;
     long long writeVal = 0;
 
-    if (icode == ICode::RMMOVQ || icode == ICode::PUSHQ || icode == ICode::CALL) {
+    if (icode == ICode::RMMOVQ || icode == ICode::PUSHQ || icode == ICode::CALL)
+    {
         write = true;
         addr = valE;
-        if (icode == ICode::CALL) writeVal = valP;
-        else writeVal = valA;
+        if (icode == ICode::CALL)
+            writeVal = valP;
+        else
+            writeVal = valA;
     }
-    else if (icode == ICode::MRMOVQ) {
+    else if (icode == ICode::MRMOVQ)
+    {
         read = true;
         addr = valE;
     }
-    else if (icode == ICode::POPQ || icode == ICode::RET) {
+    else if (icode == ICode::POPQ || icode == ICode::RET)
+    {
         read = true;
-        addr = valA; 
+        addr = valA;
     }
 
-    if (read)  valM = readLong(addr);
-    if (write) writeLong(addr, writeVal);
+    if (read)
+        valM = readLong(addr);
+    if (write)
+        writeLong(addr, writeVal);
 }
 
-// 5. 写回
-void Simulator::write_back() {
+void Simulator::write_back()
+{
     int dstE = Reg::NONE;
-    if (icode == ICode::RRMOVQ && cnd) dstE = rB; 
-    else if (icode == ICode::OPQ || icode == ICode::IRMOVQ) dstE = rB;
-    else if (icode == ICode::PUSHQ || icode == ICode::POPQ || icode == ICode::CALL || icode == ICode::RET) dstE = Reg::RSP;
+    if (icode == ICode::RRMOVQ && cnd)
+        dstE = rB;
+    else if (icode == ICode::OPQ || icode == ICode::IRMOVQ)
+        dstE = rB;
+    else if (icode == ICode::PUSHQ || icode == ICode::POPQ || icode == ICode::CALL || icode == ICode::RET)
+        dstE = Reg::RSP;
 
-    if (dstE != Reg::NONE) reg[dstE] = valE;
+    if (dstE != Reg::NONE)
+        reg[dstE] = valE;
 
     int dstM = Reg::NONE;
-    if (icode == ICode::MRMOVQ || icode == ICode::POPQ) dstM = rA;
+    if (icode == ICode::MRMOVQ || icode == ICode::POPQ)
+        dstM = rA;
 
-    if (dstM != Reg::NONE) reg[dstM] = valM;
+    if (dstM != Reg::NONE)
+        reg[dstM] = valM;
 }
 
-// 6. 更新 PC
-void Simulator::pc_update() {
-    // 【关键修改】：如果出现 HALT 或 异常，PC 保持不变，不更新
-    if (stat != Stat::AOK) {
+void Simulator::pc_update()
+{
+    if (stat != Stat::AOK)
+    {
         return;
     }
 
-    if (icode == ICode::CALL) {
+    if (icode == ICode::CALL)
+    {
         PC = valC;
     }
-    else if (icode == ICode::RET) {
+    else if (icode == ICode::RET)
+    {
         PC = valM;
     }
-    else if (icode == ICode::JXX) {
+    else if (icode == ICode::JXX)
+    {
         PC = cnd ? valC : valP;
     }
-    else {
+    else
+    {
         PC = valP;
     }
 }
 
-// --- 输出 JSON ---
-void Simulator::printJsonState(bool isFirst) {
+void Simulator::printJsonState(bool isFirst)
+{
     json j;
     j["PC"] = PC;
     j["STAT"] = stat;
@@ -270,46 +329,52 @@ void Simulator::printJsonState(bool isFirst) {
     j["CC"]["SF"] = sf ? 1 : 0;
     j["CC"]["OF"] = of ? 1 : 0;
 
-    const char* rNames[] = {"rax","rcx","rdx","rbx","rsp","rbp","rsi","rdi","r8","r9","r10","r11","r12","r13","r14"};
-    for (int i = 0; i < 15; ++i) j["REG"][rNames[i]] = reg[i];
+    const char *rNames[] = {"rax", "rcx", "rdx", "rbx", "rsp", "rbp", "rsi", "rdi", "r8", "r9", "r10", "r11", "r12", "r13", "r14"};
+    for (int i = 0; i < 15; ++i)
+        j["REG"][rNames[i]] = reg[i];
 
-    for (int i = 0; i < MEM_SIZE; i += 8) {
+    for (int i = 0; i < MEM_SIZE; i += 8)
+    {
         long long val = readLong(i);
-        if (val != 0) j["MEM"][std::to_string(i)] = val;
+        if (val != 0)
+            j["MEM"][std::to_string(i)] = val;
     }
 
-    if (!isFirst) std::cout << ",";
+    if (!isFirst)
+        std::cout << ",";
     std::cout << j << std::endl;
 }
 
-// --- 运行 ---
-void Simulator::run() {
+void Simulator::run()
+{
     std::cout << "[" << std::endl;
     bool isFirst = true;
-    while (stat == Stat::AOK) {
+    while (stat == Stat::AOK)
+    {
         fetch();
-        // 如果 Fetch 阶段就出错了（比如非法地址 ADR 或非法指令 INS），
-        // 立即打印状态并退出，此时 PC 尚未更新，指向出错指令
-        if (stat != Stat::AOK) { 
+        if (stat != Stat::AOK)
+        {
             printJsonState(isFirst);
             break;
         }
-        
+
         decode();
         execute();
         memory_access();
         write_back();
         pc_update();
-        
+
         printJsonState(isFirst);
         isFirst = false;
-        
-        if (PC < 0 || PC >= MEM_SIZE) break;
+
+        if (PC < 0 || PC >= MEM_SIZE)
+            break;
     }
     std::cout << "]" << std::endl;
 }
 
-int main() {
+int main()
+{
     Simulator sim;
     sim.loadProgram();
     sim.run();
--- cpu.h
+++ cpu.h
@@ -6,14 +6,12 @@
 #include <cstdint>
 #include <string>
 
-// 定义内存大小 (64KB，足够容纳所有测试用例)
 const int MEM_SIZE = 0x10000;
 
-// Y86 指令编码
 namespace ICode {
     const int HALT   = 0x0;
     const int NOP    = 0x1;
-    const int RRMOVQ = 0x2; // 包含 CMOVXX
+    const int RRMOVQ = 0x2;
     const int IRMOVQ = 0x3;
     const int RMMOVQ = 0x4;
     const int MRMOVQ = 0x5;
@@ -25,27 +23,25 @@ namespace ICode {
     const int POPQ   = 0xB;
 }
 
-// 寄存器 ID
 namespace Reg {
-    const int RAX = 0;
-    const int RCX = 1;
-    const int RDX = 2;
-    const int RBX = 3;
-    const int RSP = 4;
-    const int RBP = 5;
-    const int RSI = 6;
-    const int RDI = 7;
-    const int R8  = 8;
-    const int R9  = 9;
-    const int R10 = 10;
-    const int R11 = 11;
-    const int R12 = 12;
-    const int R13 = 13;
-    const int R14 = 14;
+    const int RAX = 0x0;
+    const int RCX = 0x1;
+    const int RDX = 0x2;
+    const int RBX = 0x3;
+    const int RSP = 0x4;
+    const int RBP = 0x5;
+    const int RSI = 0x6;
+    const int RDI = 0x7;
+    const int R8  = 0x8;
+    const int R9  = 0x9;
+    const int R10 = 0xA;
+    const int R11 = 0xB;
+    const int R12 = 0xC;
+    const int R13 = 0xD;
+    const int R14 = 0xE;
     const int NONE = 0xF;
 }
 
-// 状态码
 namespace Stat {
     const int AOK = 1;
     const int HLT = 2;
@@ -61,35 +57,30 @@ public:
     void run();
 
 private:
-    // --- 硬件状态 ---
     long long PC = 0;
     std::vector<unsigned char> memory;
     std::array<long long, 15> reg;
     
-    // 条件码
     bool zf = true;
     bool sf = false;
     bool of = false;
 
-    // 运行状态
     int stat = Stat::AOK;
 
-    // --- 流水线/中间变量 ---
     int icode = 0;
     int ifun = 0;
     int rA = Reg::NONE;
     int rB = Reg::NONE;
-    long long valC = 0; // 立即数
-    long long valP = 0; // 下一条指令地址 (PC + 长度)
+    long long valC = 0;
+    long long valP = 0;
     
-    long long valA = 0; // 寄存器 A 的值
-    long long valB = 0; // 寄存器 B 的值
-    long long valE = 0; // ALU 计算结果 / 有效地址
-    long long valM = 0; // 内存读取结果
+    long long valA = 0;
+    long long valB = 0;
+    long long valE = 0;
+    long long valM = 0;
 
-    bool cnd = false;   // 跳转/传送条件是否满足
+    bool cnd = false;
 
-    // --- 阶段函数 ---
     void fetch();
     void decode();
     void execute();
@@ -97,7 +88,6 @@ private:
     void write_back();
     void pc_update();
 
-    // --- 辅助工具 ---
     long long readLong(long long addr);
     void writeLong(long long addr, long long val);
     void printJsonState(bool isFirst);
